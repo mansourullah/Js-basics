73d7c90461278b3e4bfeaef7c6e5a0f61c7f4185
mansourullah <mansourazimi60@gmail.com>
2023-11-28 17:05:35 -0500

    first

---
+++ script.js
@@ -0,0 +1,78 @@
+console.log("Hello World");
+// Weird scope - Don't use 
+// var x = 5;
+// Regular old variable
+let v = 6;
+y=10; // You can change the value 
+
+
+const z = 7; // This is a constant. cant be changeed
+// z=10; // this throws and error.
+
+console.log(y,z);
+console.log(y+z);
+console.log(y-z);
+console.log(y*z);
+console.log(y/z);
+console.log(y/z); // This does division but returns the remainder
+console.log(y**z); // exponents
+
+
+let fname="Mansour";
+let lname="Azimi";
+console.log(fname + "  " + lname);
+console.log(`${ fname } ${ lname }`);
+console.log(`${ fname } ${ 5*9 }`);
+console.log( lname + 6 );
+
+
+let user = "G";
+let pass = "G001";
+
+if ( user == "G") {
+    console.log(`you are ` + user);
+
+} else {
+    console.error("DENIED")
+
+}
+
+let user = "G";
+let pass = "G001";
+
+if ( user == "G" && pass == "G001" ) 
+    console.log(`you are ` + user + and your pass is ` + pass);
+
+ else {
+    console.error("DENIED")
+
+
+const license = 18;
+const jrop = 16;
+const renal = 25;
+
+let myAge =46
+
+if (myAge < license && myAge >= JROP) {
+    console.log('you can have a jrOp License');
+
+}
+
+if (myAge>= license) {
+    console.log('you can have a regular license');
+
+}
+
+if (myAge >= rental) {
+    console.log('you csn rent a car)
+
+}
+
+if (myAge < jrOp) {
+    console.log("you're too young to drive")
+}
+
+checkAge(6);
+checkAge(17);
+checkAge(19);
+checkAge(37);
